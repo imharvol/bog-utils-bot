bc7abc629980e5271386f7c03db5ae90c2981f97
imharvol <imharvol@gmail.com>
2021-05-07 00:07:20 +0200

    Extract messages and render them using ejs
    The objective is keeping the logic of displaying the message out of the main program
    Also small improvement my executing some promises concurrently

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const path = require('path')
+const fs = require('fs')
 const { getContract, getCachedBogPrice, getEarnings, roundDecimals, bogToUsd, usdToBog, getBogBalance, getContractEvents } = require('./bogUtils')
 const { Telegraf } = require('telegraf') // https://telegraf.js.org/
 const Database = require('better-sqlite3') // https://github.com/JoshuaWise/better-sqlite3/blob/HEAD/docs/api.md
+const ejs = require('ejs')
 
 const db = new Database(path.join(__dirname, 'db.sqlite')/*, { verbose: console.log } */)
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+
+const telegramMessages = JSON.parse(fs.readFileSync('telegram-messages.json'))
 
 const sniperContractAddress = '0x8dc28ba111cde2342c083936157f6a8e53fe5514'
 
@@ -22,11 +26,9 @@ bot.start((ctx) => {
   const userRegistered = Object.values(db.prepare('SELECT EXISTS (SELECT * FROM users WHERE id = ?)').get(ctx.from.id))[0]
   if (!userRegistered) db.prepare('INSERT INTO users (id, username) VALUES (?, ?)').run(ctx.from.id, ctx.from.username)
 
-  const html = `
-Welcome, <b>${ctx.from.username}</b>!
-
-I'm a unofficial and <a href="https://github.com/imharvol/bog-utils-bot">open source</a> utility bot for <a href="http://bogtools.io/">BogTools</a> and the <a href="https://bogged.finance/">BOG token</a>. You can see what I can do for you with /help.
-  `
+  const html = ejs.render(telegramMessages['/start'], {
+    username: ctx.from.username
+  })
   ctx.replyWithHTML(html)
 })
 
@@ -36,18 +38,7 @@ I'm a unofficial and <a href="https://github.com/imharvol/bog-utils-bot">open so
  * Sends a list of commands and a description about how to use each.
  */
 bot.help((ctx) => {
-  const html = `
-- <b>/price</b> - Returns the price of BOG
-- <b>/setAddress address</b> - Sets the default address for the other commands
-- <b>/earnings [address]</b> - Returns the ammount of staked earnings
-- <b>/resume</b> -  Returns a resume of the accound and of the BOG token
-- <b>/bogToUsd ammount</b> -  Converts ammount of BOG to USD
-- <b>/usdToBog ammount</b> -  Converts ammount of USD to BOG
-- <b>/subscribe eventName [address]</b> - Subscribe to a sniper event
-- <b>/unsubscribe eventName [address]</b> - Subscribe from a sniper event
-- <b>/subscriptions</b> - Check your current subscriptions and possible events 
-- <b>/source</b> - My source code!
-  `
+  const html = telegramMessages['/help']
   ctx.replyWithHTML(html)
 })
 
@@ -59,10 +50,8 @@ bot.help((ctx) => {
 bot.command('price', async (ctx) => {
   const decimals = 4
   const bogPrice = await getCachedBogPrice(decimals)
-  const html = `
-The current rates are:
-1 BOG = <b>$${bogPrice}</b>
-  `
+
+  const html = ejs.render(telegramMessages['/price'], { bogPrice })
   ctx.replyWithHTML(html)
 })
 
@@ -74,16 +63,15 @@ The current rates are:
  */
 bot.command('setAddress', async (ctx) => {
   const messageArgs = ctx.message.text.split(' ').slice(1)
-  if (messageArgs.length !== 1) return ctx.replyWithHTML('To set your default address you need to specify a address. For example:\n<code>/setAddress 0xd7b729ef857aa773f47d37088a1181bb3fbf0099</code>')
+  if (messageArgs.length !== 1) return ctx.replyWithHTML(telegramMessages['/setAddress-no-address'])
+
   const address = messageArgs[0].toLowerCase()
 
   const userRegistered = Object.values(db.prepare('SELECT EXISTS (SELECT * FROM users WHERE id = ?)').get(ctx.from.id))[0]
-  if (!userRegistered) return ctx.replyWithHTML('Your user is not registered, please run /start to register')
+  if (!userRegistered) return ctx.replyWithHTML(telegramMessages['not-registered'])
   db.prepare('UPDATE users SET address = ? WHERE id IS ?').run(address, ctx.from.id)
 
-  const html = `
-<b>${messageArgs[0]}</b> is now your default address.
-  `
+  const html = ejs.render(telegramMessages['/setAddress'], { address })
   ctx.replyWithHTML(html)
 })
 
@@ -103,15 +91,12 @@ bot.command('earnings', async (ctx) => {
   } else if (messageArgs.length === 1) {
     address = messageArgs[0]
   }
-  if (!address) return ctx.replyWithHTML('Please provide a address like this:\n<code>/earnings 0xd7b729ef857aa773f47d37088a1181bb3fbf0099</code>\n\nYou can also set a default addres with /setAddress and then simply call /earnings.')
+  if (!address) return ctx.replyWithHTML(telegramMessages['/earnings-no-address'])
 
   const earningsBOG = await getEarnings(address, 2)
   const earningsUSD = roundDecimals(earningsBOG * await getCachedBogPrice(), 2)
 
-  const html = `
-Earnings for <b>${address}</b>:
-<b>${earningsBOG}</b> BOG = <b>$${earningsUSD}</b>
-  `
+  const html = ejs.render(telegramMessages['/earnings'], { address, earningsBOG, earningsUSD })
   ctx.replyWithHTML(html)
 })
 
@@ -123,20 +108,22 @@ Earnings for <b>${address}</b>:
  */
 bot.command('resume', async (ctx) => {
   const address = db.prepare('SELECT address FROM users WHERE id = ?').get(ctx.from.id)?.address
-  if (!address) return ctx.replyWithHTML('Please, set your address with /setAddress.')
+  if (!address) return ctx.replyWithHTML(telegramMessages['set-address'])
 
-  // TODO: This could be executed concurrently
-  const bogPrice = await getCachedBogPrice(2)
-  const stakedEarningsBog = await getEarnings(address, 2)
+  let bogPrice = getCachedBogPrice(2)
+  let stakedEarningsBog = getEarnings(address, 2)
+  let bogBalance = getBogBalance(address)
+
+  await Promise.all([bogPrice, stakedEarningsBog, bogBalance])
+
+  bogPrice = await bogPrice // Apparently we have to use await to get the result of the promise despite being resolved
+  stakedEarningsBog = await stakedEarningsBog
   const stakedEarningsUsd = roundDecimals(bogPrice * stakedEarningsBog, 2)
-  const bogBalance = await getBogBalance(address)
+  bogBalance = await bogBalance
+  const balanceBog = roundDecimals(bogBalance, 2)
+  const balanceUsd = roundDecimals(await bogToUsd(bogBalance), 2)
 
-  const html = `
-Resume for <b>${address}</b>:
-- Current BOG price: <b>$${bogPrice}</b>
-- Staked earnings: <b>${stakedEarningsBog}</b> BOG = <b>$${stakedEarningsUsd}</b>
-- Current balance: <b>${roundDecimals(bogBalance, 2)}</b> BOG = <b>$${roundDecimals(await bogToUsd(bogBalance), 2)}</b>
-  `
+  const html = ejs.render(telegramMessages['/resume'], { address, bogPrice, stakedEarningsBog, stakedEarningsUsd, balanceBog, balanceUsd })
   ctx.replyWithHTML(html)
 })
 
@@ -147,12 +134,12 @@ Resume for <b>${address}</b>:
  */
 bot.command('bogToUsd', async (ctx) => {
   const messageArgs = ctx.message.text.split(' ').slice(1)
-  if (messageArgs.length !== 1) return ctx.replyWithHTML('You need to provide the ammount of BOG that you want to convert to USD. For example:\n<code>/bogToUsd 5</code>')
-  const bog = parseFloat(messageArgs[0])
+  if (messageArgs.length !== 1) return ctx.replyWithHTML(telegramMessages['/bogToUsd-no-amount'])
+  const bogAmount = parseFloat(messageArgs[0])
 
-  const html = `
-${bog} BOG = <b>$${roundDecimals(await bogToUsd(bog), 2)}</b>
-  `
+  const usdAmount = roundDecimals(await bogToUsd(bogAmount), 2)
+
+  const html = ejs.render(telegramMessages['/bogToUsd'], { bogAmount, usdAmount })
   ctx.replyWithHTML(html)
 })
 
@@ -163,17 +150,17 @@ ${bog} BOG = <b>$${roundDecimals(await bogToUsd(bog), 2)}</b>
  */
 bot.command('usdToBog', async (ctx) => {
   const messageArgs = ctx.message.text.split(' ').slice(1)
-  if (messageArgs.length !== 1) return ctx.replyWithHTML('You need to provide the ammount of USD that you want to convert to BOG. For example:\n<code>/usdToBog 5</code>')
-  const usd = parseFloat(messageArgs[0])
+  if (messageArgs.length !== 1) return ctx.replyWithHTML(telegramMessages['/usdToBog-no-amount'])
+  const usdAmount = parseFloat(messageArgs[0])
 
-  const html = `
-$${usd} = <b>${roundDecimals(await usdToBog(usd), 2)} BOG</b>
-  `
+  const bogAmount = roundDecimals(await usdToBog(usdAmount), 2)
+
+  const html = ejs.render(telegramMessages['/usdToBog'], { usdAmount, bogAmount })
   ctx.replyWithHTML(html)
 })
 
 /**
- * /balance account
+ * /balance [account]
  *
  * Gets a account's balance. If no account is provided, the balance of the default address will be returned.
  */
@@ -185,14 +172,17 @@ bot.command('balance', async (ctx) => {
   } else if (messageArgs.length === 1) {
     address = messageArgs[0]
   }
-  if (!address) return ctx.replyWithHTML('Please provide a address like this:\n<code>/balance 0xd7b729ef857aa773f47d37088a1181bb3fbf0099</code>\n\nYou can also set a default addres with /setAddress and then simply call /balance.')
+  if (!address) return ctx.replyWithHTML(telegramMessages['/balance-no-address'])
 
-  const bogBalance = await getBogBalance(address)
+  let bogBalance = getBogBalance(address)
+  let usdBalance = bogToUsd(bogBalance)
 
-  const html = `
-Balance for <b>${address}</b>:
-<b>${roundDecimals(bogBalance, 2)}</b> BOG = <b>$${roundDecimals(await bogToUsd(bogBalance), 2)}</b>
-  `
+  await Promise.all(bogBalance, usdBalance)
+
+  bogBalance = roundDecimals(await bogBalance, 2)
+  usdBalance = roundDecimals(await usdBalance, 2)
+
+  const html = ejs.render(telegramMessages['/balance'], { address, bogBalance, usdBalance })
   ctx.replyWithHTML(html)
 })
 
@@ -214,32 +204,24 @@ bot.command('subscribe', async (ctx) => {
     address = messageArgs[1].toLowerCase()
   }
   if (!address || !eventName) {
-    return ctx.replyWithHTML(`
-Please provide a event name and address like this:
-<code>/subscribe eventName 0xd7b729ef857aa773f47d37088a1181bb3fbf0099</code>
-
-You can also set a default addres with /setAddress and then simply call
-<code>/subscribe eventName</code>
-
-Possible events: ${(await getContractEvents(sniperContractAddress)).join(', ')}
-You can also use "all" as event name and/or address.
-  `)
+    const possibleEvents = await getContractEvents(sniperContractAddress)
+    const html = ejs.render(telegramMessages['/subscribe-no-args'], { possibleEvents })
+    return ctx.replyWithHTML(html)
   }
 
   // TODO: A CHECK on the DB would be better
   // Make sure that it's not inserting a duplicate
   const alreadySubscribed = Object.values(db.prepare("SELECT EXISTS (SELECT * FROM subscriptions WHERE userId = ? AND (eventName = ? OR eventName = 'all') AND (address = ? OR address = 'all'))").get(ctx.from.id, eventName, address))[0]
-  if (alreadySubscribed) return ctx.replyWithHTML('You are already subscribed to that event/address!')
+  if (alreadySubscribed) return ctx.replyWithHTML(telegramMessages['/subscribe-already-subscribed'])
 
   // Delete duplicates from database
   if (eventName === 'all') db.prepare('DELETE FROM subscriptions WHERE userId = ? AND address = ?').run(ctx.from.id, address)
   if (address === 'all') db.prepare('DELETE FROM subscriptions WHERE userId = ? AND eventName = ?').run(ctx.from.id, eventName)
 
+  // Insert the subscription to the database
   db.prepare('INSERT INTO subscriptions (userId, eventName, address) VALUES (?, ?, ?)').run(ctx.from.id, eventName, address)
 
-  const html = `
-You have been succesfully subscribed to <b>${eventName}</b> events on <b>${address}</b>${address === 'all' ? ' addresses' : '!'}
-  `
+  const html = ejs.render(telegramMessages['/subscribe'], { eventName, address })
   ctx.replyWithHTML(html)
 })
 
@@ -252,19 +234,12 @@ bot.command('subscriptions', async (ctx) => {
   const subscriptions = db.prepare('SELECT * FROM subscriptions WHERE userId = ?').all(ctx.from.id)
 
   if (subscriptions.length === 0) {
-    return ctx.replyWithHTML(`
-You have no subscriptions!
-Try subscribing to a event using /subscribe
-  `)
+    return ctx.replyWithHTML(telegramMessages['/subscriptions-no-subscriptions'])
   }
 
-  let html = 'Your current subscriptions:\n'
+  const possibleEvents = await getContractEvents(sniperContractAddress)
 
-  for (const subscription of subscriptions) {
-    html += `- <b>${subscription.eventName}</b> events on <b>${subscription.address}</b> ${subscription.address === 'all' ? ' addresses' : ''}\n`
-  }
-
-  html += '\nPossible events: ' + (await getContractEvents(sniperContractAddress)).join(', ')
+  const html = ejs.render(telegramMessages['/subscriptions'], { subscriptions, possibleEvents })
   ctx.replyWithHTML(html)
 })
 
@@ -286,15 +261,7 @@ bot.command('unsubscribe', async (ctx) => {
     address = messageArgs[1].toLowerCase()
   }
   if (!address || !eventName) {
-    return ctx.replyWithHTML(`
-Please provide a event name and address like this:
-<code>/unsubscribe eventName 0xd7b729ef857aa773f47d37088a1181bb3fbf0099</code>
-
-You can also set a default addres with /setAddress and then simply call
-<code>/unsubscribe eventName</code>
-
-You can also use "all" as event name and/or address
-  `)
+    return ctx.replyWithHTML(telegramMessages['/unsubscribe-no-args'])
   }
   if (eventName === 'all' || address === 'all') {
     db.prepare('DELETE FROM subscriptions WHERE userId = ?').run(ctx.from.id)
@@ -304,18 +271,16 @@ You can also use "all" as event name and/or address
     db.prepare('DELETE FROM subscriptions WHERE userId = ? AND eventName = ?').run(ctx.from.id, eventName)
   } else {
     const subscribed = Object.values(db.prepare('SELECT EXISTS (SELECT * FROM subscriptions WHERE userId = ? AND eventName = ? AND address = ?)').get(ctx.from.id, eventName, address))[0]
-    if (!subscribed) return ctx.replyWithHTML('You are not subscribed to that event/address')
+    if (!subscribed) return ctx.replyWithHTML(telegramMessages['/unsubscribe-not-subscribed'])
     db.prepare('DELETE FROM subscriptions WHERE userId = ? AND eventName = ? AND address = ?').run(ctx.from.id, eventName, address)
   }
 
-  const html = `
-You have been succesfully unsubscribed from <b>${eventName}</b> events on <b>${address}</b>${address === 'all' ? ' addresses' : ''}
-  `
+  const html = ejs.render(telegramMessages['/unsubscribe'], { eventName, address })
   ctx.replyWithHTML(html)
 })
 
 bot.command('source', async (ctx) => {
-  const html = 'You can find my source code on https://github.com/imharvol/bog-utils-bot!'
+  const html = telegramMessages['/source']
   ctx.replyWithHTML(html)
 })
 
@@ -336,7 +301,7 @@ async function main () {
       "SELECT DISTINCT userId FROM subscriptions WHERE (eventName = ? AND address = ?) OR (eventName = ? AND address = 'all') OR (eventName = 'all' AND address = ?) OR (eventName = 'all' AND address = 'all')"
     ).all(eventName, address, eventName, address)
 
-    const html = `Event ${eventName} has been triggered for order id ${orderID} and owner ${address}`
+    const html = ejs.render(telegramMessages['snipe-event-triggered'], { eventName, orderID, address })
 
     for (const subscriber of subscribers) {
       bot.telegram.sendMessage(subscriber.userId, html)
